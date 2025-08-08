78f5574c32a28190c92d03ded6430d064be2a895
Alex White <alex.eliz.white@gmail.com>
2025-08-07 21:28:57 -0400

    Initial Commit

---
+++ HomePractice/HomePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomePracticeApp.swift
+//  HomePractice
+//
+//  Created by Alex White on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct HomePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
